6e2170ef898daf9f0a50b31e14e8d71d1e39fe09
antonpage1302 <anton1302d.enisenko@gmail.com>
2026-02-12 16:09:38 +0300

    Code

--- py.py
+++ py.py
@@ -1,2 +1,29 @@
+import time
 print("flashStarter")
-input("enter to exit")
+input("enter to continue")
+print("import flashing")
+time.sleep(1)
+print("testing...")
+print("tested succes")
+print("importing sucess")
+print("connecting")
+print("flashed!")
+print("start")
+print("from flashing import windows")
+cd = input("cd choose a,b,c:")
+input(cd)
+input("this y/n?")
+print("loading ")
+I =
+"""    _____          __               __________               __   
+  /  _  \   _____/  |_  ____   ____\______   \ ____   _____/  |_ 
+ /  /_\  \ /    \   __\/  _ \ /    \|    |  _//  _ \ /  _ \   __\
+/    |    \   |  \  | (  <_> )   |  \    |   (  <_> |  <_> )  |  
+\____|__  /___|  /__|  \____/|___|  /______  /\____/ \____/|__|  
+        \/     \/                 \/       \/                    """
+print(I)
+while True:
+     print("loading.")
+     print("loading..")
+     print("loading...")
+     print("load")
